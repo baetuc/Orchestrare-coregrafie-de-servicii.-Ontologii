7228e6a707e0c0f9a467aed908bb89a1fce3e06a
unknown <dlucaniuc@dlucaniuc-lpt.ias.bitdefender.biz>
2016-04-11 11:03:26 +0300

    Am separat workflow-ul de colectia de functii

---
+++ Server/ProcedureCollection/Server/routes/getinfo.js
@@ -0,0 +1,21 @@
+function GetInfo(){
+	
+	var getLocatie = function(lat,long,callback){
+		request.get('127.0.0.1:3232?lat=' + locatie.lat + '&long='  + locatie.long,function(strada){
+			callback(strada);
+		});
+	}
+
+	var getVreme = function(streetName,callback){
+		request.get('127.0.0.1:3232?streetName=' + streetName,function(vreme){
+			callback(vreme);
+		});
+	}
+
+	var getStiri = function(streetName,callback){
+		request.get('127.0.0.1:3232?streetName=' + streetName,function(stiri){
+			callback(stiri);
+		});
+	}
+}
+module.exports = getInfo;
