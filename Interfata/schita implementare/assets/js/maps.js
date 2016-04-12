7c91fb9aafa4f03295d9f3f5effd0b5c5f677d1a
sebastianAlbisteanu <albisteanu.sebastian@yahoo.com>
2016-04-12 18:09:10 +0300

    interface skeleton

---
+++ Interfata/schita implementare/assets/js/maps.js
@@ -0,0 +1,7 @@
+		function initMap() {
+		    var mapDiv = document.getElementById('map');
+		    var map = new google.maps.Map(mapDiv, {
+		      center: {lat: 47.17, lng: 27.57},
+		      zoom: 8
+		    });
+	  	}
