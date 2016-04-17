72a80475f0f7237e26b1c60023df77cb1a7a8c3c
cykablyeter <flegmuci@gmail.com>
2016-04-17 16:52:18 +0300

    getEventDays + getEvents

--- Services/Module-Calendar/src/ro/calendar/provider/CalendarProvider.java
+++ Services/Module-Calendar/src/ro/calendar/provider/CalendarProvider.java
@@ -2,6 +2,8 @@ package ro.calendar.provider;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class CalendarProvider {
 	public static ArrayList<Event> CalendarEvents =new ArrayList<Event>();
@@ -11,16 +13,47 @@ public class CalendarProvider {
 	 * @param 	day	A timestamp inside the day the list of events is requested in
 	 * @return	The list of events taking place in this day
 	 */
-	public ArrayList<Event> getEvents(Timestamp day) { return null; }
-	
-	
+	public ArrayList<Event> getEvents(Timestamp day) { 
+		ArrayList<Event>toReturn=new ArrayList<Event>();
+		Calendar cObj1 =Calendar.getInstance();
+		Calendar cObj2 =Calendar.getInstance();
+		cObj1.setTime(day);
+		for(Event e:CalendarEvents){
+			cObj2.setTime(e.getStartTime());
+			if(cObj1.get(Calendar.DAY_OF_MONTH)==cObj1.get(Calendar.DAY_OF_MONTH)&&
+					cObj1.get(Calendar.MONTH)==cObj1.get(Calendar.MONTH)&&
+					cObj1.get(Calendar.YEAR)==cObj1.get(Calendar.YEAR)){
+						toReturn.add(e);
+			}
+		}
+		if(toReturn.isEmpty()){
+			return null;
+		}
+		return toReturn;
+	}
 	/**
 	 * Returns a list of days in the specified month that have events.
 	 * 
 	 * @param 	month	A timestamp inside the requested month
 	 * @return	A list of timestamps inside the days when events take place
 	 */
-	public ArrayList<Timestamp> getEventDays(Timestamp month) { return null; }
+	public ArrayList<Timestamp> getEventDays(Timestamp month) { 
+		ArrayList<Timestamp>toReturn=new ArrayList<Timestamp>();
+		Calendar cObj1 =Calendar.getInstance();
+		Calendar cObj2 =Calendar.getInstance();
+		cObj1.setTime(month);
+		for(Event e:CalendarEvents){
+			cObj2.setTime(e.getStartTime());
+			if(cObj1.get(Calendar.MONTH)==cObj1.get(Calendar.MONTH)&&
+					cObj1.get(Calendar.YEAR)==cObj1.get(Calendar.YEAR)){
+						toReturn.add(e.getStartTime());
+			}
+		}
+		if(toReturn.isEmpty()){
+			return null;
+		}
+		return toReturn;
+	}
 	
 	
 	/**
